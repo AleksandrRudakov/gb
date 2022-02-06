32053eed942f8de8c974d15126d606e918e790fe
shmel_ras <shmel_ras@mail.ru>
2022-02-06 16:14:17 +0300

    Практическое задание по уроку 3

---
+++ less-3/less-3-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace less_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+                { 1, 5, 2, 8, 7 },
+                { 2, 3, 7, 8, 9 },
+                { 4, 0, 2, 1, 7 },
+                { 1, 5, 8, 3, 2 },
+                { 4, 0, 5, 1, 7 }
+            };
+
+            string d1 = "";
+            string d2 = "";
+
+            for (int i = 0; i < matrix.GetLength(0) ; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                    if (i==j)
+                    {
+                        d1 = d1 + Convert.ToString(matrix[i, j]) + " ";
+                    }
+                    if ((i+j) == (matrix.GetLength(0)-1))
+                    {
+                        d2 = Convert.ToString(matrix[i, j]) + " " + d2;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine($"d1 = {d1}");
+            Console.WriteLine($"d2 = {d2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ less-3/less-3-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace less_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matrix = {
+                { "Иванов", "8(000)000-00-00" },
+                { "Петров", "petrov@petrov.ru" },
+                { "Сидоров", "8(111)111-11-11" },
+                { "Сергеев", "sergeev@sergeev.ru" },
+                { "Алексеев", "8(222)222-22-22" }
+            };
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
