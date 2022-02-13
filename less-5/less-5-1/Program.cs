fe9361e8087efa1bd4e1183ba7401935f0e2d825
shmel_ras <shmel_ras@mail.ru>
2022-02-13 23:19:07 +0300

    Практическое задание к уроку 5

---
+++ less-5/less-5-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace less_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный текст:");
+            string str = Console.ReadLine();
+            File.WriteAllText("test.txt", str);
+            Console.WriteLine("Введенный текст сохранен в файл test.txt в директории проекта");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ less-5/less-5-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace less_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("startup.txt", Convert.ToString(DateTime.Now) + Environment.NewLine);
+            Console.WriteLine("Текущее время добавлено в файл startup.txt");
+            Console.ReadKey();
+        }
+    }
+}
